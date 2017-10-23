82ab1a01cc13230e16d97570682c6707e4b011dd
Georg Göri <georg@makeclean.org>
2017-10-23 19:42:19 -0400

    update for 2017

--- feed.php
+++ feed.php
@@ -11,32 +11,34 @@ $channel = new Channel();
 $channel
 ->title("Ö1 ".$_GET["title"])
 ->description("Converted RSS from 7 Tage Ö1")
-->url('http://oe1.orf.at/konsole')
+->url('http://oe1.orf.at/player')
 ->language('de')
-->copyright('Copyright 2014')
+->copyright('Copyright 2017')
 ->pubDate(time())
 ->lastBuildDate(time())
 ->ttl(60)
 ->appendTo($feed);
 
+$daysProgramm=json_decode(file_get_contents("https://audioapi.orf.at/oe1/api/json/current/broadcasts"));
 
-for ($offset = 0; $offset < 7; $offset++) {
-$streamDate=date_sub(new DateTime(NULL),new DateInterval("P".$offset."D"));
-$daysProgramm=json_decode(file_get_contents("http://oe1.orf.at/programm/konsole/tag/".$streamDate->format("Ymd")));
-foreach($daysProgramm->list as $show)
+foreach($daysProgramm as $day)
 {
-if(preg_match("/^".$_GET["title"]."$/",$show->title) ){
+foreach($day->broadcasts as $show_header)
+{
+$title = isset($show_header->programTitle) ? $show_header->programTitle : $show_header->title; 
+
+if(preg_match("/^".$_GET["title"]."$/",$title) ){
+$show=json_decode(file_get_contents($show_header->href));
 $item = new Item();
-$itemDate=DateTime::createFromFormat("d.m.YH:i",$show->day_label.$show->time);
 $item
-->title($show->title)
+->title($title)
 ->description($show->info)
-->enclosure($show->url_stream)
-->pubDate($itemDate->getTimestamp())
+->enclosure("http://loopstream01.apa.at/?channel=oe1&shoutcast=0&player=oe1_v1&referer=radiothek&id=".$show->streams[0]->loopStreamId)
+->pubDate($show_header->niceTime / 1000)
 ->guid($show->id, true)
 ->appendTo($channel);
 }
 }
-
 }
+
 echo $feed;
